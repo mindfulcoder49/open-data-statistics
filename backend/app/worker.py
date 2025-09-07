7a56fdb679c7e0c51ffc3c9db051bc4ea9281a71
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2025-09-07 09:20:55 -0400

    update docker-compose.dev for only one celery worker

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -3,4 +3,4 @@ from app.tasks import app
 # This file is the entry point for the Celery worker.
 # To run the worker, you would use the command:
 
-celery -A app.worker worker --loglevel= info --concurrency=1
+#celery -A app.worker worker --loglevel= info --concurrency=1

48c07d969fa6106509cbaeaeadfb6e0dab98834d
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2025-09-07 09:15:26 -0400

    update worker concurrency to make it 1

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -2,4 +2,5 @@ from app.tasks import app
 
 # This file is the entry point for the Celery worker.
 # To run the worker, you would use the command:
-# celery -A app.worker worker --loglevel=info
+
+celery -A app.worker worker --loglevel= info --concurrency=1
